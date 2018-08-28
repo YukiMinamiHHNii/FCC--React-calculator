c3bbbd51a98b879863b917c498d3007a0b569c42
yukiminamihhnii <homu.homu.nii@gmail.com>
2018-08-27 20:17:27 -0500

    Adds components and layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
 import "./App.css";
+import Calcpad from "./Calcpad";
 
 class App extends Component {
+	constructor(props){
+		super(props);
+		this.state={
+			value: 0
+		}
+	}
   render() {
     return (
-      <div className="App"/>
+      <div className="app">
+      	<div id="calc">
+      		<div id="display">{this.state.value}</div>
+      		<Calcpad/>
+      	</div>
+      </div>
     );
   }
 }
---
+++ src/Calcpad.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+class Calcpad extends React.Component{
+	render(){
+		return (
+			<div id="pad">
+				<button id="equals" className="operator">=</button>
+      	<button id="zero" className="number">0</button>
+      	<button id="one" className="number">1</button>
+      	<button id="two" className="number">2</button>
+      	<button id="three" className="number">3</button>
+      	<button id="four" className="number">4</button>
+      	<button id="five" className="number">5</button>
+      	<button id="six" className="number">6</button>
+      	<button id="seven" className="number">7</button>
+      	<button id="eight" className="number">8</button>
+      	<button id="nine" className="number">9</button>
+      	<button id="add" className="operator">+</button>
+      	<button id="substract" className="operator">-</button>
+      	<button id="multiply" className="operator">*</button>
+      	<button id="divide" className="operator">/</button>
+      	<button id="decimal" className="operator">.</button>
+      	<button id="clear" className="operator">Clear</button>
+			</div>
+		);
+	}
+}
+
+export default Calcpad;
