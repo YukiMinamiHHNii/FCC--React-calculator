ac2a2ff72f29e75af2a8a4f5f2618e8b792e2228
yukiminamihhnii <homu.homu.nii@gmail.com>
2018-08-28 21:21:27 -0500

    Adds some logic for queuing and general behavior

--- src/App.js
+++ src/App.js
@@ -3,22 +3,79 @@ import "./App.css";
 import Calcpad from "./Calcpad";
 
 class App extends Component {
-	constructor(props){
+	constructor(props) {
 		super(props);
-		this.state={
-			value: 0
-		}
+		this.state = {
+			input: "0",
+			output: "",
+			nums: [],
+			ops: []
+		};
+		this.clear = this.clear.bind(this);
+		this.queueOperator = this.queueOperator.bind(this);
+		this.calculate = this.calculate.bind(this);
 	}
-  render() {
-    return (
-      <div className="app">
-      	<div id="calc">
-      		<div id="display">{this.state.value}</div>
-      		<Calcpad/>
-      	</div>
-      </div>
-    );
-  }
+	queueOperator(data) {
+		this.setState(prevState => {
+			if (data.type === "num") {
+				return this.state.input.length === 1 && this.state.input === "0"
+					? { input: data.value, output: prevState.output }
+					: {
+							input: prevState.input.concat(data.value),
+							output: prevState.output
+					  };
+			} else {
+				if (data.value === ".") {
+					return !prevState.input.includes(".")
+						? {
+								input: prevState.input.concat(data.value),
+								output: prevState.output
+						  }
+						: "";
+				} else {
+					return {
+						input: "0",
+						output: prevState.output.concat(prevState.input.concat(data.value)),
+						//concat is used on the array because pop returns a new one instead of modifying the existent
+						//could have used the spread operator as well
+						nums: prevState.nums.concat(prevState.input), 
+						ops: prevState.ops.concat(data.value)
+					};
+				}
+			}
+		});
+	}
+	clear() {
+		return this.setState({
+			input: "0",
+			output: ""
+		});
+	}
+	calculate() {
+		this.setState(prevState => {
+			return {
+				input: "0",
+				output: ""
+			};
+		});
+	}
+	render() {
+		return (
+			<div className="app">
+				<div id="calc">
+					<div id="display">
+						<h4>{this.state.output}</h4>
+						<h3>{this.state.input}</h3>
+					</div>
+					<Calcpad
+						input={this.queueOperator}
+						clear={this.clear}
+						calculate={this.calculate}
+					/>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/Calcpad.js
+++ src/Calcpad.js
@@ -1,26 +1,33 @@
 import React from "react";
 
 class Calcpad extends React.Component{
+	constructor(props){
+		super(props);
+		this.queueOnClick= this.queueOnClick.bind(this);
+	}
+	queueOnClick(e){
+		this.props.input({value: e.target.value, type: new RegExp("\\d").test(e.target.value)?"num":"op"});
+	}
 	render(){
 		return (
 			<div id="pad">
-      	<button id="zero" className="number">0</button>
-      	<button id="one" className="number">1</button>
-      	<button id="two" className="number">2</button>
-      	<button id="three" className="number">3</button>
-      	<button id="four" className="number">4</button>
-      	<button id="five" className="number">5</button>
-      	<button id="six" className="number">6</button>
-      	<button id="seven" className="number">7</button>
-      	<button id="eight" className="number">8</button>
-      	<button id="nine" className="number">9</button>
-      	<button id="decimal" className="number">.</button>
-      	<button id="add" className="operator">+</button>
-      	<button id="substract" className="operator">-</button>
-      	<button id="multiply" className="operator">*</button>
-      	<button id="divide" className="operator">/</button>
-      	<button id="equals" className="operator">=</button>
-      	<button id="clear" className="operator">Clear</button>
+      	<button id="zero" className="number" value="0" onClick={this.queueOnClick}>0</button>
+      	<button id="one" className="number" value="1" onClick={this.queueOnClick}>1</button>
+      	<button id="two" className="number" value="2" onClick={this.queueOnClick}>2</button>
+      	<button id="three" className="number" value="3" onClick={this.queueOnClick}>3</button>
+      	<button id="four" className="number" value="4" onClick={this.queueOnClick}>4</button>
+      	<button id="five" className="number" value="5" onClick={this.queueOnClick}>5</button>
+      	<button id="six" className="number" value="6" onClick={this.queueOnClick}>6</button>
+      	<button id="seven" className="number" value="7" onClick={this.queueOnClick}>7</button>
+      	<button id="eight" className="number" value="8" onClick={this.queueOnClick}>8</button>
+      	<button id="nine" className="number" value="9" onClick={this.queueOnClick}>9</button>
+      	<button id="decimal" className="number" value="." onClick={this.queueOnClick}>.</button>
+      	<button id="add" className="operator" value="+" onClick={this.queueOnClick}>+</button>
+      	<button id="substract" className="operator" value="-" onClick={this.queueOnClick}>-</button>
+      	<button id="multiply" className="operator" value="*" onClick={this.queueOnClick}>*</button>
+      	<button id="divide" className="operator" value="/" onClick={this.queueOnClick}>/</button>
+      	<button id="equals" className="operator" value="=" onClick={this.props.calculate}>=</button>
+      	<button id="clear" className="operator" value="cls" onClick={this.props.clear}>Clear</button>
 			</div>
 		);
 	}
